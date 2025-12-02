bd9fb6327a7b83c8c0d8c41132422326b82a1e98
Maksym Davydenko <maksydenko@gmail.com>
2025-12-02 21:51:08 +0200

    Fix vite.config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,19 +1,23 @@
-import { resolve } from 'node:path';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { devtools } from '@tanstack/devtools-vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    devtools(),
+    tanstackRouter({
+      autoCodeSplitting: true,
+      target: 'react'
+    }),
+    viteReact()
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true
   }
 });
